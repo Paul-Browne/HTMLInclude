1607c931704cc522ef8f210a0a8c76511e1f0579
paul browne <paulmatthewbrowne@gmail.com>
2019-05-24 01:07:54 +0300

    1.1.1

--- HTMLInclude.js
+++ HTMLInclude.js
@@ -1,62 +1,58 @@
-/*! HTMLInclude v1.1.0 | MIT License | github.com/paul-browne/HTMLInclude */ 
-! function(w,d) {
+/*! HTMLInclude v1.1.1 | MIT License | github.com/paul-browne/HTMLInclude */ 
+!function(w, d) {
     if (!w.HTMLInclude) {
         w.HTMLInclude = function() {
             function isInViewport(element, offset) {
                 return element.getBoundingClientRect().top <= (+offset + w.innerHeight);
             }
-            function callback(response, elements) {
-                elements.forEach(function(element) {
-                    var dataReplace = element.getAttribute("data-replace");
-                    var z = response;
-                    if (dataReplace) {
-                        dataReplace.split(",").forEach(function(el) {
-                            var o = el.split(":");
-                            z = response.replace(new RegExp(o[0].trim(), "g"), o[1].trim());
-                        });
-                    }
-                    if (element.parentNode) {
-                        element.outerHTML = z;
-                        var scripts = new DOMParser().parseFromString(z, 'text/html').querySelectorAll("SCRIPT");
-                        var i = 0;
-                        var j = scripts.length;
-                        while (i < j) {
-                            var newScript = d.createElement("SCRIPT");
-                            scripts[i].src ? newScript.src = scripts[i].src : newScript.innerHTML = scripts[i].innerHTML;
-                            d.head.appendChild(newScript);
-                            i++;
-                        }
-                    }
-                });
-            }
-            function ajax(url, els) {
+            function ajax(url, elements) {
                 var xhr = new XMLHttpRequest();
                 xhr.onreadystatechange = function() {
                     if (xhr.readyState == 4 && xhr.status == 200) {
-                        callback(xhr.responseText, els);
+                        elements.forEach(function(element) {
+                            var dataReplace = element.getAttribute("data-replace");
+                            var z = xhr.responseText;
+                            if (dataReplace) {
+                                dataReplace.split(",").forEach(function(el) {
+                                    var o = el.trim().split(":");
+                                    z = z.replace(new RegExp(o[0], "g"), o[1]);
+                                });
+                            }
+                            element.outerHTML = z;
+                            var scripts = new DOMParser().parseFromString(z, 'text/html').querySelectorAll("SCRIPT");
+                            var i = 0;
+                            var j = scripts.length;
+                            while (i < j) {
+                                var newScript = d.createElement("SCRIPT");
+                                scripts[i].src ? newScript.src = scripts[i].src : newScript.innerHTML = scripts[i].innerHTML;
+                                d.head.appendChild(newScript);
+                                i++;
+                            }
+                        });
                     }
                 };
                 xhr.open("GET", url, true);
                 xhr.send();
             }
-            function lazyLoad(element, offset){
-                w.addEventListener("scroll", function scrollFunc(){
-                    if( isInViewport(element, offset) ){
+            function lazyLoad(element, offset) {
+                w.addEventListener("scroll", function scrollFunc() {
+                    if (isInViewport(element, offset)) {
                         w.removeEventListener("scroll", scrollFunc);
                         ajax(element.getAttribute("data-include"), [element]);
                     }
                 })
             }
             var store = {};
-            var dis = d.querySelectorAll('[data-include]');
+            var dis = d.querySelectorAll('[data-include]:not([data-in])');
             var i = dis.length;
             while (i--) {
                 var di = dis[i].getAttribute('data-include');
                 var laziness = dis[i].getAttribute('data-lazy');
-                if( !laziness || ( laziness && isInViewport(dis[i], laziness) ) ){
+                dis[i].setAttribute("data-in", "");
+                if (!laziness || (laziness && isInViewport(dis[i], laziness))) {
                     store[di] = store[di] || [];
                     store[di].push(dis[i]);
-                }else{
+                } else {
                     lazyLoad(dis[i], laziness);
                 }
             }
